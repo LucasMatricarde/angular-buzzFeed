1699779759ee0de9409b5d712af8de4d59410158
lucas-linux <lucasmatricarde@gmail.com>
2025-03-02 13:07:29 -0300

    Tirando os acentos, criando a logica da tela

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json";
 
 @Component({
   selector: 'app-quiz',
@@ -23,6 +24,45 @@ export class QuizComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title;
+
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length - 1;
+    }
   }
 
+  playerChoose(value:string){
+    this.answers.push(value);
+    this.nextQuestion();
+  }
+
+  async nextQuestion(){
+    this.questionIndex+=1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    }else{
+      const finalAnswers:string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected = quiz_questions.results[finalAnswers as keyof typeof quiz_questions.results];
+    }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, index, arr) => {
+      if(arr.filter(item => item === previous).length >
+         arr.filter(item => item === current).length){
+          return previous;
+      }else{
+        return current;
+      }
+    })
+
+    return result;
+  }
 }
